2d539528db1cfe46a799b783e6fef5446330be00
[dyad] <bruno.fmonte@gmail.com>
2026-02-09 15:41:26 -0300

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-Login -> Prime -> VocalSandbox -> Duel.">
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
