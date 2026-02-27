6bd23e34d01d873b41a9f8df3da259ccd2d25922
[dyad] <bruno.fmonte@gmail.com>
2026-02-27 16:46:59 -0300

    Updated src/services/youtubeService.ts

--- src/services/youtubeService.ts
+++ src/services/youtubeService.ts
@@ -1,8 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// const firebaseConfig = {
- apiKey: "AIzaSyDmJaH9jZUqI2fGQV_yAYC-8w8t3RmvrlI",
+const firebaseConfig = {
+// apiKey: "AIzaSyDmJaH9jZUqI2fGQV_yAYC-8w8t3RmvrlI",
   authDomain: "karaoke-prime-4f59e.firebaseapp.com",
   projectId: "karaoke-prime-4f59e",
   storageBucket: "karaoke-prime-4f59e.firebasestorage.app",
