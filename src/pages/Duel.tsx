fc4e17db98bf22a2da2ee3239e23858f68d08e66
[dyad] <bruno.fmonte@gmail.com>
2026-02-28 12:41:55 -0300

    Updated src/pages/Duel.tsx

--- src/pages/Duel.tsx
+++ src/pages/Duel.tsx
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
- // apiKey: "AIzaSyBaCJPLU9kL_Ufu4S2yJX2v5up6vp5R548",
+  apiKey: "AIzaSyBaCJPLU9kL_Ufu4S2yJX2v5up6vp5R548",
   authDomain: "karaoke-prime-4f59e.firebaseapp.com",
   projectId: "karaoke-prime-4f59e",
   storageBucket: "karaoke-prime-4f59e.firebasestorage.app",
