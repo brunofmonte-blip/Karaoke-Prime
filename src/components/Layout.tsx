531d3d4db4719641ee571a6e28ebd305c1b2f414
[dyad] <bruno.fmonte@gmail.com>
2026-03-03 09:59:58 -0300

    Updated src/components/Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,6 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import BottomNavigationBar from './BottomNavigationBar';
-import { cn } from '@/lib/utils';
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-interface LayoutProps {
-  children: React.ReactNode;
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
-    <div className="min-h-screen flex flex-col bg-background">
-      <Header />
-      <main className="flex-grow pb-16 md:pb-0">
-        {children}
-      </main>
-      <Footer />
-      <BottomNavigationBar />
-    </div>
-  );
-};
-
-export default Layout;
