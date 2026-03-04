4de9f0d08c4edde1c67f03b978ca6ab06f4b2293
[dyad] <bruno.fmonte@gmail.com>
2026-03-04 13:40:12 -0300

    Updated src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-// Configuração fictícia para destravar a interface
-const firebaseConfig = {
-  apiKey: "dummy-api-key-para-destravar-tela",
-  authDomain: "karaoke-prime-dummy.firebaseapp.com",
-  projectId: "karaoke-prime-dummy",
-  storageBucket: "karaoke-prime-dummy.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider(); 
+// Assinatura Única: Limpeza Definitiva - Linha 1: export const firebaseConfig = {};
+export const firebaseConfig = {};
+export const auth = {}; 
+export const db = {};
+export const googleProvider = {};
+export default firebaseConfig;
