6cdfed22d05d8f25028086af15ca9c23d1d13191
[dyad] <bruno.fmonte@gmail.com>
2026-03-05 11:06:05 -0300

    Updated src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,23 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./globals.css";
+import React from 'react';
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Se você tiver componentes de Header ou Footer, pode importá-los aqui no futuro.
+// import Header from './Header';
+// import BottomNavigationBar from './BottomNavigationBar';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col bg-background">
+      {/* <Header /> */}
+      <main className="flex-grow w-full relative">
+        {children}
+      </main>
+      {/* <BottomNavigationBar /> */}
+    </div>
+  );
+};
+
+export default Layout;
