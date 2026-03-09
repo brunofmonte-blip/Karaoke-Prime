558af2aa3e9d45009e00b846c969e451cbd3d7b5
[dyad] <bruno.fmonte@gmail.com>
2026-03-09 19:31:29 -0300

    Updated src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -18,4 +18,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider();
+export const googleProvider = new GoogleAuthProvider(); 
