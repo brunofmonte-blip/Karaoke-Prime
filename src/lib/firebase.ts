3e64e6b87ab121f08524fbaa83eb6c3d019e79e0
[dyad] <bruno.fmonte@gmail.com>
2026-03-10 13:50:32 -0300

    Updated src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // 1. As suas chaves públicas entram aqui (pode usar strings diretas ou import.meta.env se estiver a usar variáveis de ambiente):
 const firebaseConfig = {
-  apiKey: "A_SUA_API_KEY",
-  authDomain: "O_SEU_AUTH_DOMAIN",
-  projectId: "O_SEU_PROJECT_ID",
-  storageBucket: "O_SEU_STORAGE_BUCKET",
-  messagingSenderId: "O_SEU_MESSAGING_SENDER_ID",
-  appId: "O_SEU_APP_ID",
-  measurementId: "O_SEU_MEASUREMENT_ID"
+  apiKey: "AIzaSyDmJaH9jZUqI2fGQV_yAYC-8w8t3RmvrlI",
+  authDomain: "karaoke-prime-4f59e.firebaseapp.com",
+  projectId: "karaoke-prime-4f59e",
+  storageBucket: "karaoke-prime-4f59e.firebasestorage.app",
+  messagingSenderId: "1019002450289",
+  appId: "1:1019002450289:web:fd3801c55b5bdeb2c906d0",
+  measurementId: "G-H3J972N7FM"
 };
 
 // 2. Inicializa a Aplicação
