ff5d044e43cfc8a696d5da9c5a044de4f16e69a5
[dyad] <bruno.fmonte@gmail.com>
2026-03-10 20:38:52 -0300

    Updated src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,8 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Index from "./pages/Index";
-import Login from "./pages/Login";
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const queryClient = new QueryClient();
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Falha ao encontrar o elemento root");
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Index />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App; 
+createRoot(rootElement).render(<App />);
