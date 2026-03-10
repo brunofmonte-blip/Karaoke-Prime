74794e185d94e83517fba23abcba9ae81ae96513
[dyad] <bruno.fmonte@gmail.com>
2026-03-10 20:32:17 -0300

    Updated src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Falha ao encontrar o elemento root");
+
+createRoot(rootElement).render(<App />);
