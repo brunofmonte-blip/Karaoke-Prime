36d4ceec5a621147ed6146ff435e2b53dd8b7ed4
[dyad] <bruno.fmonte@gmail.com>
2026-03-11 11:12:56 -0300

    Updated src/pages/Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import { useNavigate } from 'react-router-dom';
 import { 
   Search, Mic2, GraduationCap, Star, Lock, Music, LayoutDashboard, 
