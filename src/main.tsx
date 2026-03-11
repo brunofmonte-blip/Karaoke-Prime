6ab39835f7c2be0ebeffbd1e5e40b3b5b9d2382b
[dyad] <bruno.fmonte@gmail.com>
2026-03-10 21:06:34 -0300

    Updated src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-
-// 📍 REMOVIDO: import './index.css' (pois o arquivo não existe)
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './globals.css'; 
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Falha ao encontrar o elemento root");
 
-createRoot(rootElement).render(<App />);
+createRoot(rootElement).render(<App />); 
