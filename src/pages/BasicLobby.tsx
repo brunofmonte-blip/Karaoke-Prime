c5b767705a84d4d56aab207e3cb6cae4025e8318
[dyad] <bruno.fmonte@gmail.com>
2026-03-19 10:02:42 -0300

    Updated src/pages/BasicLobby.tsx

--- src/pages/BasicLobby.tsx
+++ src/pages/BasicLobby.tsx
@@ -15,7 +15,7 @@ export default function BasicLobby() {
 
   const defaultSongs = [
     { title: "NÃO QUERO DINHEIRO", artist: "TIM MAIA", videoId: "5eJwf_k68r8" },
-    { title: "LET IT BE", artist: "THE BEATLES", videoId: "QDYfEBY9NM4" },
+    { title: "LET IT BE", artist: "THE BEATLES", videoId: "OCdB7yFeg00" },
     { title: "AMOR MAIOR", artist: "JOTA QUEST", videoId: "bA8yA4wVf4A" },
     { title: "EVIDÊNCIAS", artist: "CHITÃOZINHO E XORORÓ", videoId: "ePjtnSPFWK8" },
     { title: "SHALLOW", artist: "LADY GAGA", videoId: "bo_efYhYU2A" },
